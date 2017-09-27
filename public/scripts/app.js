af5232c77795dd543dd6e16026d7d63673434477
Jakedalus <jacquedalus@gmail.com>
2017-09-27 18:23:32 -0400

    Add basic functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,16 +8,40 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+marked.setOptions({
+    sanitize: true
+});
+
 var MarkdownApp = function (_React$Component) {
     _inherits(MarkdownApp, _React$Component);
 
-    function MarkdownApp() {
+    function MarkdownApp(props) {
         _classCallCheck(this, MarkdownApp);
 
-        return _possibleConstructorReturn(this, (MarkdownApp.__proto__ || Object.getPrototypeOf(MarkdownApp)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (MarkdownApp.__proto__ || Object.getPrototypeOf(MarkdownApp)).call(this, props));
+
+        _this.handleTyping = _this.handleTyping.bind(_this);
+        _this.state = {
+            inputText: '',
+            outputText: { __html: '' }
+        };
+        return _this;
     }
 
     _createClass(MarkdownApp, [{
+        key: 'handleTyping',
+        value: function handleTyping(e) {
+            var inputText = e.target.value;
+            var markedText = marked(inputText);
+
+            this.setState(function () {
+                return {
+                    inputText: inputText,
+                    outputText: { __html: markedText }
+                };
+            });
+        }
+    }, {
         key: 'render',
         value: function render() {
             return React.createElement(
@@ -26,18 +50,27 @@ var MarkdownApp = function (_React$Component) {
                 React.createElement(
                     'h1',
                     null,
-                    'Markdown Viewrs'
+                    'Markdown Viewer'
                 ),
-                React.createElement(
-                    'p',
-                    null,
-                    'What'
-                )
+                React.createElement(Input, { handleTyping: this.handleTyping }),
+                React.createElement(Output, { outputText: this.state.outputText })
             );
         }
     }]);
 
     return MarkdownApp;
 }(React.Component);
+
+var Input = function Input(props) {
+    return React.createElement(
+        'div',
+        { id: 'input' },
+        React.createElement('textarea', { onInput: props.handleTyping, rows: '10', cols: '50', name: 'inputText' })
+    );
+};
+
+var Output = function Output(props) {
+    return React.createElement('div', { id: 'output', dangerouslySetInnerHTML: props.outputText });
+};
 
 ReactDOM.render(React.createElement(MarkdownApp, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,15 +1,58 @@
 
+marked.setOptions({
+    sanitize: true 
+});
+
 class MarkdownApp extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.handleTyping = this.handleTyping.bind(this);
+        this.state = {
+            inputText: '',
+            outputText: {__html: ''}
+        };
+    }
+    
+    handleTyping(e) {
+        var inputText = e.target.value;
+        var markedText = marked(inputText);
+
+        this.setState(() => {
+            return {
+                inputText: inputText,
+                outputText: {__html: markedText}
+            } 
+        });
+    }
     
     render() {
         return (
             <div>
-                <h1>Markdown Viewrs</h1>
-                <p>What</p>
+                <h1>Markdown Viewer</h1>
+                <Input handleTyping={this.handleTyping}/>
+                <Output outputText={this.state.outputText} />
             </div>
         );
     }
     
 }
+
+const Input = (props) => {
+    return (
+        <div id='input'>
+            <textarea onInput={props.handleTyping} rows="10" cols="50" name="inputText"></textarea>
+        </div>
+    );
+};
+
+const Output = (props) => {
+    return (
+        <div id='output' dangerouslySetInnerHTML={props.outputText}>
+            
+        </div>
+    );
+};
 
 ReactDOM.render(<MarkdownApp />, document.getElementById('app'));
